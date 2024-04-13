fd677138fc7a975c9bc4b6d6bd5c5212a7684870
hamballah2024 <166895455+hamballah2024@users.noreply.github.com>
2024-04-13 16:20:26 +0800

    Update sc.py

--- sc.py
+++ sc.py
@@ -6,9 +6,13 @@ import requests
 def maxstream_command(update, context):
     if len(context.args) > 0:
         url = context.args[0]
+
+        # Menunjukkan bahwa bot sedang memproses permintaan
+        context.bot.send_message(chat_id=update.message.chat_id, text="Sedang memproses...", reply_to_message_id=update.message.message_id)
+        
         processed_result = process_maxstream_url(url)
         reply_message = f"*MPD Maxstream :* {url}\n\n*PSSH Maxstream :* `{processed_result}`"
-        context.bot.send_message(chat_id=update.message.chat_id, text=reply_message, parse_mode=ParseMode.MARKDOWN)
+        context.bot.send_message(chat_id=update.message.chat_id, text=reply_message, parse_mode=ParseMode.MARKDOWN, reply_to_message_id=update.message.message_id)
     else:
         context.bot.send_message(chat_id=update.message.chat_id, text="Silakan masukkan URL setelah perintah /maxstream.")
 
@@ -16,9 +20,13 @@ def maxstream_command(update, context):
 def vision_command(update, context):
     if len(context.args) > 0:
         url = context.args[0]
+
+        # Menunjukkan bahwa bot sedang memproses permintaan
+        context.bot.send_message(chat_id=update.message.chat_id, text="Sedang memproses...", reply_to_message_id=update.message.message_id)
+        
         processed_result = process_vision_url(url)
         reply_message = f"*MPD Vision :* {url}\n\n*PSSH Vision :* `{processed_result}`"
-        context.bot.send_message(chat_id=update.message.chat_id, text=reply_message, parse_mode=ParseMode.MARKDOWN)
+        context.bot.send_message(chat_id=update.message.chat_id, text=reply_message, parse_mode=ParseMode.MARKDOWN, reply_to_message_id=update.message.message_id)
     else:
         context.bot.send_message(chat_id=update.message.chat_id, text="Silakan masukkan URL setelah perintah /vision.")
 
