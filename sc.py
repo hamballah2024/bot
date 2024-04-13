45999e898db06ef0e723723659bea67d9850da94
hamballah2024 <166895455+hamballah2024@users.noreply.github.com>
2024-04-13 15:48:44 +0800

    Update sc.py

--- sc.py
+++ sc.py
@@ -4,11 +4,8 @@ import requests
 
 # Fungsi untuk menangani perintah /maxstream
 def maxstream_command(update, context):
-    # Memeriksa apakah perintah memiliki argumen URL
     if len(context.args) > 0:
-        # Mendapatkan URL dari argumen perintah
         url = context.args[0]
-        # Memproses URL Maxstream
         processed_result = process_maxstream_url(url)
         reply_message = f"*MPD Maxstream :* {url}\n\n*PSSH Maxstream :* `{processed_result}`"
         context.bot.send_message(chat_id=update.message.chat_id, text=reply_message, parse_mode=ParseMode.MARKDOWN)
@@ -17,16 +14,38 @@ def maxstream_command(update, context):
 
 # Fungsi untuk menangani perintah /vision
 def vision_command(update, context):
-    # Memeriksa apakah perintah memiliki argumen URL
     if len(context.args) > 0:
-        # Mendapatkan URL dari argumen perintah
         url = context.args[0]
-        # Memproses URL Vision
         processed_result = process_vision_url(url)
         reply_message = f"*MPD Vision :* {url}\n\n*PSSH Vision :* `{processed_result}`"
         context.bot.send_message(chat_id=update.message.chat_id, text=reply_message, parse_mode=ParseMode.MARKDOWN)
     else:
         context.bot.send_message(chat_id=update.message.chat_id, text="Silakan masukkan URL setelah perintah /vision.")
+
+# Fungsi untuk menangani perintah /getmpdcubmu
+def getmpdcubmu_command(update, context):
+    if len(context.args) > 1:
+        option = context.args[0]
+        value = context.args[1]
+        
+        # Menunjukkan bahwa bot sedang memproses permintaan
+        processing_message = context.bot.send_message(chat_id=update.message.chat_id, text="Sedang memproses...", reply_to_message_id=update.message.message_id)
+        
+        processed_result = process_get_mpd(option, value)
+        
+        # Mengirim hasil
+        context.bot.send_message(chat_id=update.message.chat_id, text=processed_result, parse_mode=ParseMode.MARKDOWN, reply_to_message_id=update.message.message_id)
+        
+        # Menghapus pesan "Sedang memproses..." setelah hasil dikirim
+        context.job_queue.run_once(delete_message, 0, context=[update.message.chat_id, processing_message.message_id])
+
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text="Silakan masukkan opsi (name atau id) dan nilai setelah perintah /getmpdcubmu.", reply_to_message_id=update.message.message_id)
+
+# Fungsi untuk menghapus pesan "Sedang memproses..."
+def delete_message(context):
+    chat_id, processing_message_id = context.job.context
+    context.bot.delete_message(chat_id=chat_id, message_id=processing_message_id)
 
 # Fungsi untuk memproses URL Maxstream
 def process_maxstream_url(url):
@@ -38,23 +57,43 @@ def process_vision_url(url):
     processed_url = requests.get("https://cendolcen.my.id/tools/script/pssh-maxstream-bot/vision.php?mpd=" + url)
     return processed_url.text
 
+# Fungsi untuk memproses MPD submu
+def process_get_mpd(option, value):
+    if option == 'name':
+        processed_url = requests.get(f"https://cendolcen.my.id/tools/script/pssh-maxstream-bot/cari-cubmu-channel.php?name={value}")
+    elif option == 'id':
+        processed_url = requests.get(f"https://cendolcen.my.id/tools/script/pssh-maxstream-bot/cari-cubmu-channel.php?id={value}")
+    else:
+        return "Opsi tidak valid."
+    
+    data = processed_url.json()  # Mengubah respons ke format JSON
+    if 'ID' in data:
+        formatted_result = (
+            f"ID: {data['ID']}\n"
+            f"Nama: {data['Name']}\n"
+            f"Kategori: {data['SubName']}\n"
+            f"MPD URL: {data['IP']}\n"
+            f"Image URL: {data['Image URL']}"
+        )
+        return formatted_result
+    else:
+        return "Data tidak ditemukan atau format respons salah."
+
 def main():
-    # Token bot Telegram Anda
     token = '7129313428:AAFP1ELIdIqJ37Sx94eDGHb35Vn5on26kW4'
 
-    # Membuat objek updater dan dispatcher
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
 
-    # Menambahkan handler untuk perintah /maxstream
     maxstream_handler = CommandHandler('maxstream', maxstream_command)
     dispatcher.add_handler(maxstream_handler)
 
-    # Menambahkan handler untuk perintah /vision
     vision_handler = CommandHandler('vision', vision_command)
     dispatcher.add_handler(vision_handler)
 
-    # Memulai polling untuk bot Anda
+    getmpdcubmu_handler = CommandHandler('getmpdcubmu', getmpdcubmu_command)
+    dispatcher.add_handler(getmpdcubmu_handler)
+
     updater.start_polling()
     updater.idle()
 
